863bcb37849fe673b2522d49d508bb8a2f9801e7
Moises P. Sena <moisespsena@gmail.com>
2018-10-16 08:27:19 -0300

    Add header comments

--- cmd/tunneld/main.go
+++ cmd/tunneld/main.go
@@ -1,3 +1,7 @@
+// Copyright (C) 2017 Michał Matczuk
+// Use of this source code is governed by an AGPL-style
+// license that can be found in the LICENSE file.
+
 package main
 
 import "github.com/mmatczuk/go-http-tunnel/cli/tunneld"
